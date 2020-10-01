0e0a27c0a7dc20d632c35eba8de5ac185f40991e
Hongwei Xi <gmhwxi@gmail.com>
2020-09-30 20:37:24 -0400

    Updating: very very minorly

---
+++ build.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+######
+#
+# Author:
+# Richar Kent (https://github.com/sparverius)
+#
+######
+
+make_all() {
+    make -C srcgen all
+}
+
+######
+#
+# For when this repository is cloned without --recursive flag
+#
+######
+
+clone_xanadu() {
+    if [ ! -d xanadu/srcgen ]; then
+	git clone https://github.com/xanadu-lang/xanadu.git
+    fi
+}
+
+######
+
+XATS2JS_SCRIPT='#!/bin/sh
+
+XATS2JS='${PWD}/bin/xats2js'
+
+if [ ! "$XATSHOME" ] ; then
+  export XATSHOME='${PWD}/xanadu'
+fi
+
+if [ -f "$XATS2JS" ] ; then
+  "$XATS2JS" "$@"
+else
+  # if build failed or "make cleanall" was executed
+  echo "please execute `make all` to build [xats2js]"
+fi
+'
+generate_script() {
+    printf "${XATS2JS_SCRIPT}" > ./bin/xats2js.sh
+}
+
+######
+
+main() {
+    clone_xanadu
+    make_all
+    generate_script
+}
+
+main
+
+###### end of [build.sh] ######
