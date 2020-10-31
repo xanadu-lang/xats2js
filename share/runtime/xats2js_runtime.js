3b6c762925dc20c3f364fef1995b53ac1a478579
Hongwei Xi <gmhwxi@gmail.com>
2020-10-31 16:27:10 -0400

    Updating: very very minorly

---
+++ share/runtime/xats2js_runtime.js
@@ -0,0 +1,1006 @@
+
+/* ****** ****** */
+/*
+Runtime for Xats2js
+*/
+/* ****** ****** */
+var
+XATS2JS_top = null
+var
+XATS2JS_none = null
+var
+XATS2JS_void = null
+/* ****** ****** */
+var // global
+XATS2JS_excbas = 0
+var // global
+XATS2JS_exctag = 0
+function
+XATS2JS_new_exctag
+  (    )
+{
+var bas0 =
+XATS2JS_excbas;
+var tag1 =
+XATS2JS_exctag + 1;
+XATS2JS_exctag = tag1;
+return ( bas0 + tag1 );
+}
+/* ****** ****** */
+function
+XATS2JS_char
+  ( cs )
+{
+// cs: singleton
+return cs.charCodeAt(0);
+}
+function
+XATS2JS_string
+  ( cs )
+{
+return cs;//cs:JS_string
+}
+/* ****** ****** */
+function
+XATS2JS_fcast
+  ( x0 )
+{
+return x0; //obj: object
+}  
+/* ****** ****** */
+//
+function
+XATS2JS_raise
+  ( exn )
+{
+  throw exn;//no return!
+}
+function
+XATS2JS_reraise
+  ( exn )
+{
+  throw exn;//no return!
+}
+/* ****** ****** */
+//
+function
+XATS2JS_assert
+  ( b0 )
+{
+if
+(!b0)
+{
+  // [b0] is false
+  throw new Error();
+} // if-then
+return ; // [b0] is true
+}
+//
+function
+XATS2JS_assertloc
+  ( b0, loc )
+{
+if
+(!b0)
+{
+  // [b0] is false
+  throw new Error(loc);
+} // if-then
+return ; // [b0] is true
+}
+function
+XATS2JS_assertmsg
+  ( b0, msg )
+{
+if
+(!b0)
+{
+  // [b0] is false
+  throw new Error(msg);
+} // if-then
+return ; // [b0] is true
+}
+//
+/* ****** ****** */
+function
+XATS2JS_lval_err
+  (loc)
+{
+  // non-left-val!
+  throw new Error(loc);
+}
+/* ****** ****** */
+function
+XATS2JS_lval_get
+  (lvl0)
+{
+//
+var
+root = lvl0.root;
+var
+offs = lvl0.offs;
+//
+if
+(
+lvl0.hasOwnProperty('prev')
+)
+root =
+XATS2JS_lval_get(lvl0.prev);
+//  
+return root[offs]; // JS_lval_get
+}
+/* ****** ****** */
+function
+XATS2JS_lval_set
+  (lvl0, obj1)
+{
+//
+var root = null;
+var offs = lvl0.offs;
+//
+if
+(
+lvl0.hasOwnProperty('prev')
+)
+{ // flat tuple
+root =
+XATS2JS_lval_get(lvl0.prev);
+root =
+root.slice(); root[offs] = obj1;
+XATS2JS_lval_set(lvl0.prev, root);
+}
+else
+{
+  lvl0.root[offs] = obj1; // boxed
+}
+return; // end of [XATS2JS_lval_set]
+}
+/* ****** ****** */
+function
+XATS2JS_patckerr0()
+{
+  throw new Error();
+}
+function
+XATS2JS_patckerr1(loc)
+{
+  throw new Error(loc);
+}
+/* ****** ****** */
+function
+XATS2JS_matcherr0()
+{
+  throw new Error();
+}
+function
+XATS2JS_matcherr1(loc)
+{
+  throw new Error(loc);
+}
+/* ****** ****** */
+function
+XATS2JS_new_var0
+  ()
+{
+return {root:[null], offs:0};
+}
+function
+XATS2JS_new_var1
+  (init)
+{
+return {root:[init], offs:0};
+}
+/* ****** ****** */
+function
+XATS2JS_new_cofs
+  (lvl1, idx2)
+{
+return {root:lvl1, offs:idx2}; 
+}
+function
+XATS2JS_new_tofs
+  (lvl1, idx2)
+{
+return {root:lvl1, offs:idx2}; 
+}
+/* ****** ****** */
+function
+XATS2JS_new_cptr
+  (lvl1, idx2)
+{
+var
+con1 =
+XATS2JS_lval_get(lvl1);
+return {root:con1, offs:idx2}; 
+}
+function
+XATS2JS_new_tptr
+  (lvl1, idx2)
+{
+var
+tup1 =
+XATS2JS_lval_get(lvl1);
+if
+(tup1[0] >= 0)
+return {
+  root:tup1, offs:idx2
+};
+else
+return {
+  prev:lvl1,
+  root:tup1, offs:idx2
+}; // end of [XATS2JS_new_tptr]
+}
+/* ****** ****** */
+
+function
+XATS2JS_new_lazy
+  (thunk)
+{
+return {
+  lztag: 0 // unused
+, lzval: null // saved
+, lzfun: thunk // for eval
+}
+} // end of [XATS2JS_new_lazy]
+
+function
+XATS2JS_lazy_eval
+  (lzobj)
+{
+//
+var lzres;
+//
+if
+(lzobj.lztag===0)
+{
+lzres =
+lzobj.lzfun();
+lzobj.lztag = 1;
+lzobj.lzval = lzres;
+} else
+{
+//
+// HX: tracking
+//
+  lzobj.lztag += 1;
+  lzres = lzobj.lzval;
+} ;
+return lzres; // lazy_eval
+} // end of [XATS2JS_lazy_eval]
+
+/* ****** ****** */
+
+function
+XATS2JS_new_llazy
+  (thunk, frees)
+{
+return {
+  lztag: false // unused
+, lzfun: thunk // for eval
+, lzfrs: frees // for frees
+}
+} // end of [XATS2JS_new_llazy]
+
+function
+XATS2JS_llazy_eval
+  (lzobj)
+{
+//
+if
+(lzobj.lztag)
+{
+  throw new Error();
+}
+//
+  lzobj.lztag = true; // used
+  return lzobj.lzfun(); // eval
+//
+} // end of [XATS2JS_llazy_eval]
+
+function
+XATS2JS_llazy_free
+  (lzobj)
+{
+//
+if
+(lzobj.lztag)
+{
+  throw new Error();
+}
+//
+  lzobj.lztag = true; // used
+  return lzobj.lzfrs(); // free
+//
+} // end of [XATS2JS_llazy_free]
+
+/* ****** ****** */
+//
+// prelude/bool.sats
+//
+/* ****** ****** */
+function
+XATS2JS_bool_neg
+  (b0)
+{ return !b0 ; }
+/* ****** ****** */
+function
+XATS2JS_bool_add
+  (b1, b2)
+{ return (b1 || b2); }
+function
+XATS2JS_bool_mul
+  (b1, b2)
+{ return (b1 && b2); }
+/* ****** ****** */
+//
+// prelude/char.sats
+//
+/* ****** ****** */
+function
+XATS2JS_char_eqzq
+  (c0)
+{
+return (0===c0); // eqzq
+}
+function
+XATS2JS_char_neqzq
+  (c0)
+{
+return (0!==c0); // neqzq
+}
+/* ****** ****** */
+function
+XATS2JS_char_cmp
+  (c1, c2)
+{
+if
+(c1 < c2)
+return (-1);
+else
+return (c1 <= c2 ? 0 : 1);
+}
+/* ****** ****** */
+function
+XATS2JS_char_equal
+  (c1, c2)
+{
+return (c1===c2); // equal
+}
+function
+XATS2JS_char_noteq
+  (c1, c2)
+{
+return (c1!==c2); // noteq
+}
+/* ****** ****** */
+//
+// prelude/gint.sats
+//
+/* ****** ****** */
+function
+XATS2JS_gint_abs_sint
+  (x0)
+{
+if
+(x0 >= 0)
+{
+  return x0; // abs
+}
+else
+{
+  return -x0; // abs
+}
+}
+/* ****** ****** */
+function
+XATS2JS_gint_neg_sint
+  (x0)
+{
+return (-x0); // neg
+}
+/* ****** ****** */
+function
+XATS2JS_gint_succ_sint
+  (x0)
+{
+return (x0 + 1); // +1
+}
+function
+XATS2JS_gint_pred_sint
+  (x0)
+{
+return (x0 - 1); // -1
+}
+/* ****** ****** */
+function
+XATS2JS_gint_lt_sint_sint
+  (x1, x2)
+{
+return (x1 < x2); // lt
+}
+function
+XATS2JS_gint_gt_sint_sint
+  (x1, x2)
+{
+return (x1 > x2); // gt
+}
+function
+XATS2JS_gint_eq_sint_sint
+  (x1, x2)
+{
+return (x1 === x2); // eq
+}
+function
+XATS2JS_gint_lte_sint_sint
+  (x1, x2)
+{
+return (x1 <= x2); // lte
+}
+function
+XATS2JS_gint_gte_sint_sint
+  (x1, x2)
+{
+return (x1 >= x2); // gte
+}
+function
+XATS2JS_gint_neq_sint_sint
+  (x1, x2)
+{
+return (x1 !== x2); // neq
+}
+/* ****** ****** */
+function
+XATS2JS_gint_cmp_sint_sint
+  (x1, x2)
+{
+if
+(x1 < x2)
+return (-1);
+else
+return (x1 <= x2 ? 0 : 1);
+}
+/* ****** ****** */
+function
+XATS2JS_gint_add_sint_sint
+  (x1, x2)
+{
+   return (x1 + x2); // add
+}
+/* ****** ****** */
+function
+XATS2JS_gint_sub_sint_sint
+  (x1, x2)
+{
+  return (x1 - x2); // sub
+}
+/* ****** ****** */
+function
+XATS2JS_gint_mul_sint_sint
+  (x1, x2)
+{
+  return (x1 * x2); // mul
+}
+/* ****** ****** */
+function
+XATS2JS_gint_mod_sint_sint
+  (x1, x2)
+{
+  return (x1 % x2); // mod
+}
+/* ****** ****** */
+function
+XATS2JS_gint_div_sint_sint
+  (x1, x2)
+{ 
+//
+  var q0 = x1 / x2;
+//
+  if
+  (q0 >= 0)
+  {
+    return Math.floor(q0);
+  }
+  else
+  {
+    return Math.ceil( q0 );
+  }
+}
+/* ****** ****** */
+//
+// prelude/gflt.sats
+//
+/* ****** ****** */
+function
+XATS2JS_gflt_i_dflt
+  (x0)
+{
+  return x0; // int
+}
+/* ****** ****** */
+function
+XATS2JS_gflt_abs_dflt
+  (x0)
+{
+if
+(x0 >= 0.0)
+{
+  return x0; // abs
+}
+else
+{
+  return -x0; // abs
+}
+}
+/* ****** ****** */
+function
+XATS2JS_gflt_neg_dflt
+  (x0)
+{
+return (-x0); // neg
+}
+/* ****** ****** */
+function
+XATS2JS_gflt_succ_dflt
+  (x0)
+{
+return (x0 + 1); // +1
+}
+function
+XATS2JS_gflt_pred_dflt
+  (x0)
+{
+return (x0 - 1); // -1
+}
+/* ****** ****** */
+function
+XATS2JS_gflt_lt_dflt_dflt
+  (x1, x2)
+{
+return (x1 < x2); // lt
+}
+function
+XATS2JS_gflt_gt_dflt_dflt
+  (x1, x2)
+{
+return (x1 > x2); // gt
+}
+function
+XATS2JS_gflt_eq_dflt_dflt
+  (x1, x2)
+{
+return (x1 === x2); // eq
+}
+function
+XATS2JS_gflt_lte_dflt_dflt
+  (x1, x2)
+{
+return (x1 <= x2); // lte
+}
+function
+XATS2JS_gflt_gte_dflt_dflt
+  (x1, x2)
+{
+return (x1 >= x2); // gte
+}
+function
+XATS2JS_gflt_neq_dflt_dflt
+  (x1, x2)
+{
+return (x1 !== x2); // neq
+}
+/* ****** ****** */
+function
+XATS2JS_gflt_cmp_dflt_dflt
+  (x1, x2)
+{
+if
+(x1 < x2)
+return (-1);
+else
+return (x1 <= x2 ? 0 : 1);
+}
+/* ****** ****** */
+function
+XATS2JS_gflt_add_dflt_dflt
+  (x1, x2)
+{
+   return (x1 + x2); // add
+}
+/* ****** ****** */
+function
+XATS2JS_gflt_sub_dflt_dflt
+  (x1, x2)
+{
+   return (x1 - x2); // sub
+}
+/* ****** ****** */
+function
+XATS2JS_gflt_mul_dflt_dflt
+  (x1, x2)
+{
+   return (x1 * x2); // mul
+}
+/* ****** ****** */
+function
+XATS2JS_gflt_div_dflt_dflt
+  (x1, x2)
+{
+   return (x1 / x2); // div
+}
+/* ****** ****** */
+//
+// prelude/string.sats
+//
+/* ****** ****** */
+//
+// HX-2020-09-28:
+// Please note that:
+// A string is a JS_string
+// A string_vt is a JS_array
+//
+/* ****** ****** */
+function
+XATS2JS_string_vt2t
+  (cs)
+{
+cs.pop(); // remove the last '0'
+var cs =
+String.fromCharCode.apply(null, cs);
+return cs; // XATS2JS_string_vt2t
+}
+/* ****** ****** */
+function
+XATS2JS_string_lt
+  (x1, x2)
+{
+  return (x1 < x2);
+}
+function
+XATS2JS_string_gt
+  (x1, x2)
+{
+  return (x1 > x2);
+}
+function
+XATS2JS_string_eq
+  (x1, x2)
+{
+  return (x1===x2);
+}
+function
+XATS2JS_string_lte
+  (x1, x2)
+{
+  return (x1 <= x2);
+}
+function
+XATS2JS_string_gte
+  (x1, x2)
+{
+  return (x1 >= x2);
+}
+function
+XATS2JS_string_neq
+  (x1, x2)
+{
+  return (x1 !== x2);
+}
+/* ****** ****** */
+function
+XATS2JS_string_cmp
+  (x1, x2)
+{
+if(x1 < x2)
+{
+  return -1;
+} else
+{
+  return (x1===x2 ? 0 : 1);
+} // end of [if]
+}
+/* ****** ****** */
+function
+XATS2JS_string_head_opt
+  (cs)
+{
+if
+(cs.length <= 0)
+{
+  return 0; // none
+}
+return cs.charCodeAt(0);
+}
+/* ****** ****** */
+function
+XATS2JS_string_head_raw
+  (cs)
+{
+return cs.charCodeAt(0);
+}
+/* ****** ****** */
+function
+XATS2JS_string_tail_raw
+  (cs)
+{
+return cs.slice(1);//tail
+}
+/* ****** ****** */
+function
+XATS2JS_string_get_at
+  (cs, i0)
+{
+  return cs[i0];
+  // cs: JS_array(char)
+}
+/* ****** ****** */
+function
+XATS2JS_strtmp_vt_alloc
+  (bsz)
+{
+  cs =
+  new Array(bsz+1);
+  cs[bsz] = 0; return cs;
+}
+function
+XATS2JS_strtmp_vt_set_at
+  (cs, i0, c0)
+{
+  cs[i0] = c0;
+  return;//cs:JS_array(char)
+}
+/* ****** ****** */
+function
+XATS2JS_string_forall_cfr
+  (cs, f0)
+{
+var
+res = true;
+var
+len = cs.length
+for
+( i0 = 0
+; i0 < len; i0 += 1)
+{
+var c0 = cs.charCodeAt(i0);
+if
+(!f0(c0)){res = false; break;}
+}
+return res; // string_forall_cfr
+}
+/* ****** ****** */
+function
+XATS2JS_string_rforall_cfr
+  (cs, f0)
+{
+var
+res = true;
+var
+len = cs.length
+for
+( i0 = len
+; i0 >= 1 ; i0 -= 1)
+{
+var c0 = cs.charCodeAt(i0-1);
+if
+(!f0(c0)){res = false; break;}
+}
+return res; // string_rforall_cfr
+}
+/* ****** ****** */
+function
+XATS2JS_string_vt_forall_cfr
+  (cs, f0)
+{
+var
+res = true;
+var
+len = cs.length
+for
+( i0 = 0
+; i0 < len; i0 += 1)
+{
+if
+(!f0(cs[i0])){res = false; break;}
+}
+return res; // string_vt_forall_cfr
+}
+/* ****** ****** */
+function
+XATS2JS_string_vt_rforall_cfr
+  (cs, f0)
+{
+var
+res = true;
+var
+len = cs.length;
+for
+( i0 = len
+; i0 >= 1 ; i0 -= 1)
+{
+if
+(!f0(cs[i0-1])){res = false; break;}
+}
+return res; // string_vt_rforall_cfr
+}
+/* ****** ****** */
+//
+// prelude/array.sats
+// (arrays of
+//  various dimensionality)
+//
+/* ****** ****** */
+//
+// HX: 1-dimensional
+//
+/* ****** ****** */
+
+function
+XATS2JS_a0ptr_make
+  (x0)
+{
+var A0 =
+XATS2JS_a0ptr_alloc();
+A0[0] = x0; return A0;
+}
+function
+XATS2JS_a0ptr_alloc
+  (  )
+{
+  return new Array(1);
+}
+
+/* ****** ****** */
+
+function
+XATS2JS_a0ref_get
+  (A0)
+{
+  return A0[0];
+}
+
+/* ****** ****** */
+
+function
+XATS2JS_a0ref_set
+  (A0, x0)
+{
+  return (A0[0] = x0);
+}
+
+/* ****** ****** */
+//
+// HX: 1-dimensional
+//
+/* ****** ****** */
+
+function
+XATS2JS_a1ptr_alloc
+  (asz)
+{
+  return new Array(asz);
+}
+
+/* ****** ****** */
+
+function
+XATS2JS_a1ref_get_at
+  (A0, i0)
+{
+  return A0[i0];
+}
+function
+XATS2JS_a1ptr_get_at
+  (A0, i0)
+{
+  return A0[i0];
+}
+
+/* ****** ****** */
+
+function
+XATS2JS_a1ref_set_at
+  (A0, i0, x0)
+{
+  return (A0[i0] = x0);
+}
+function
+XATS2JS_a1ptr_set_at
+  (A0, i0, x0)
+{
+  return (A0[i0] = x0);
+}
+function
+XATS2JS_a1ptr_set_at_raw
+  (A0, i0, x0)
+{
+  return (A0[i0] = x0);
+}
+
+/* ****** ****** */
+//
+// prelude/unsafe.sats
+//
+/* ****** ****** */
+//
+function
+XATS2JS_UN_p2tr_get
+  (ptr)
+{
+  return XATS2JS_lval_get(ptr);
+}
+function
+XATS2JS_UN_p2tr_set
+  (ptr, obj)
+{
+  XATS2JS_lval_set(ptr, obj); return;
+}
+//
+/* ****** ****** */
+
+/* end of [XATS2JS_runtime.cats] */
+/* ****** ****** */
+/*
+G_print for Xats2js/Node
+*/
+/* ****** ****** */
+//
+function
+XATS2JS_NODE_g_print(obj)
+{
+var
+rep = obj.toString();
+process.stdout.write(rep);
+return; // XATS2JS_NODE_g_print
+}
+//
+/* ****** ****** */
+function
+XATS2JS_NODE_bool_print
+  (b0)
+{
+if(b0)
+{
+XATS2JS_NODE_g_print("true");
+}
+else
+{
+XATS2JS_NODE_g_print("false");
+}
+return; // XATS2JS_NODE_bool_print
+}
+/* ****** ****** */
+function
+XATS2JS_NODE_char_print
+  (c0)
+{
+// c0: number
+XATS2JS_NODE_g_print
+(String.fromCharCode(c0));
+return; // XATS2JS_NODE_char_print
+}
+/* ****** ****** */
+function
+XATS2JS_NODE_gint_print_sint
+  (x0)
+{
+XATS2JS_NODE_g_print(x0);
+return; // gint_print_sint
+}
+function
+XATS2JS_NODE_gint_print_uint
+  (x0)
+{
+XATS2JS_NODE_g_print(x0);
+return; // gint_print_uint
+}
+/* ****** ****** */
+function
+XATS2JS_NODE_string_print
+  (cs)
+{
+  return XATS2JS_NODE_g_print(cs);
+}
+/* ****** ****** */
+
+/* end of [XATS2JS_NODE_g_print.cats] */
