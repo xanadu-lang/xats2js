5b2ebf9259f4662e505f5c4f701dd5ab56dcde44
Hongwei Xi <gmhwxi@gmail.com>
2020-11-08 15:11:20 -0500

    Updating: very very minorly

---
+++ share/runtime/xats2js_xatslib_libc.js
@@ -0,0 +1,30 @@
+/* ****** ****** */
+/*
+XATS2JS_XATSLIB_LIBC
+*/
+/* ****** ****** */
+
+/* ****** ****** */
+/*
+Basics for Xats2js/Node
+*/
+/* ****** ****** */
+
+const
+XATS2JS_NODE_fs =
+require( 'fs' ) ;
+
+/* ****** ****** */
+
+function
+XATS2JS_NODE_fs_readFileSync
+  (path)
+{
+  return String(
+    XATS2JS_NODE_fs.readFileSync(path)
+  ); // end of [return]
+}
+
+/* ****** ****** */
+
+/* end of [xatslib_libc_XATS2JS_NODE_prelude.cats] */
